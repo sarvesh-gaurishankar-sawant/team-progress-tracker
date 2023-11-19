7e91db174f5d11dcabf3049d470cf7a9faaf5fbf
Tejashree Gore <gore.te@northeastern.edu>
2023-11-19 14:22:53 -0500

    add user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,53 @@
+import * as userService from '../services/user-service.js';
+import mongoose from 'mongoose';
+import { setResponse, setErrorResponse } from './response-handler.js';  
+
+export const createUser = async (req, res) => {
+    try {
+        const reqBody = {...req.body};
+        const user = await userService.createUser(reqBody);
+        setResponse(user, res);
+    } catch (error) {
+        setErrorResponse(error, res);
+    }
+}
+
+export const getUsers = async (req, res) => {
+    try {
+        const user = await userService.getUsers();
+        setResponse(user, res);
+    } catch (error) {
+        setErrorResponse(error, res);
+    }
+}
+
+export const findUserById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await userService.findUserById(id);
+        setResponse(user, res);
+    } catch (error) {
+        setErrorResponse(error, res);
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const reqBody = {...req.body};
+        const user = await userService.updateUser(reqBody, id);
+        setResponse(user, res);
+    } catch (error) {
+        setErrorResponse(error, res);
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await userService.deleteUser(id);
+        setResponse({ "message": "User deleted successfully."}, res);
+    } catch (error) {
+        setErrorResponse(error, res);
+    }
+}
