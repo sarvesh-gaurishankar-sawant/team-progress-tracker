aecc1fc85c311069a143e41fe3cc41d19415522a
Tejashree Gore <gore.te@northeastern.edu>
2023-11-19 14:25:00 -0500

    initialize app and connect with mongodb

---
+++ app.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import registerRouter from './routes/index.js'
+import models from './models/index.js'
+const app = express();
+
+const initialize = (app) => {
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded());
+    mongoose.connect('mongodb+srv://info6150user:qucHuj-4fegzy-kyptad@info6150fall2023.urnad.mongodb.net/kanban?retryWrites=true&w=majority');
+    registerRouter(app);
+}
+
+export default initialize;
