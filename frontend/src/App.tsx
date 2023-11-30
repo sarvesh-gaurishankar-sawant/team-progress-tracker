1e7f972eb54cd259444e54dc2e243a7e1ef77fe7
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-11-29 19:12:40 -0500

    Create Board component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Board from "./components/Board";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      {<Board />}
     </h1>
   )
 }
