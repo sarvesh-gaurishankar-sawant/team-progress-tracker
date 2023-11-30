1af6154250b3eed396c2bf23cf1c6d2f1578f538
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-11-29 21:45:48 -0500

    Add function to add new column to state

--- frontend/src/components/Boards/Board.tsx
+++ frontend/src/components/Boards/Board.tsx
@@ -1,13 +1,27 @@
 import { useState } from "react"
 import EmptyBoard from "./EmptyBoard"
 
+type Column = {
+  index: number;
+  title: string;
+}
+
 export default function Board() {
 
-  const [columns, setColumns] = useState([]);
+  const [columns, setColumns] = useState<Column[]>([]);
 
   return (
     <div>   
-        {columns.length === 0 && <EmptyBoard />}
+        {columns.length === 0 && <EmptyBoard createNewColumn={createNewColumn} />}
     </div>  
   )
+
+  function createNewColumn(){
+    const columnIndex: number = columns.length;
+    const newColumn: Column = {
+      index: columnIndex,
+      title: `Column Title ${columnIndex}`
+    }
+    setColumns([...columns, newColumn]);
+  }
 }
---
+++ frontend/src/components/Boards/Column.tsx
@@ -0,0 +1,5 @@
+export default function Column() {
+  return (
+    <div>Column</div>
+  )
+}
