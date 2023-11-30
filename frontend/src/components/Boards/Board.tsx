d44ee147868d06024b5989610fa99bd12b1449bf
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-11-30 00:31:56 -0500

    Create a create new task function

--- frontend/src/components/Boards/Board.tsx
+++ frontend/src/components/Boards/Board.tsx
@@ -8,13 +8,20 @@ type Column = {
   title: string;
 }
 
+type Task = {
+  index: number;
+  title: string;
+  columnName: string;
+}
+
 export default function Board() {
 
   const [columns, setColumns] = useState<Column[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   return (
     <div className="overflow-x-auto h-screen">   
-        <Button key="add_new_column" className="w-72 border border-sky-500" onClick={() => {createNewColumn()}}>Add new tasks</Button>
+        <Button key="add_new_column" className="w-72 border border-sky-500" onClick={() => {createNewTask()}}>Add new tasks</Button>
         {columns.length === 0 && <EmptyBoard createNewColumn={createNewColumn} />}
         {columns.length !== 0 && <DisplayColumn columns={columns} createNewColumn={createNewColumn}/>}
     </div>  
@@ -28,4 +35,17 @@ export default function Board() {
     }
     setColumns([...columns, newColumn]);
   }
+
+  function createNewTask() {
+    const taskIndex: number = tasks.length;
+    const newTask: Task = {
+      index: taskIndex,
+      title: `Task Title ${taskIndex}`,
+      columnName: columns[0]?.title
+    }
+    setTasks([...tasks, newTask]);
+    console.log(tasks)
+  }
+
+  
 }
