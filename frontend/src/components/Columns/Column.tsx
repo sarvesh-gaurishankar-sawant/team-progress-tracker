e4b5c30da3f21f27ff5ef3f7e41e697b6d3f0df8
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-11-30 20:34:43 -0500

    Display tasks title

--- frontend/src/components/Columns/Column.tsx
+++ frontend/src/components/Columns/Column.tsx
@@ -16,6 +16,7 @@ export default function Column({ column }: Props) {
       title: "Task 1",
       description: "Description for Task 1",
       status: "Todo",
+      subtask: ["615cf2b9dbf66b029028a7e5", "615cf2b9dbf66b029028a7e6"]
   },
   {
       _id:"615cf2b0dbf66b029028a7e2",
@@ -23,20 +24,41 @@ export default function Column({ column }: Props) {
       title: "Task 2",
       description: "Description for Task 2",
       status: "Doing",
+      subtask: ["615cf2b9dbf66b029028a7e7"]
   },
   {
       _id:"615cf2b9dbf66b029028a7e3",
+      index: 3,
       title: "Task 3",
       description: "Description for Task 3",
       status: "Pending",
+      subtask: []
   }]
 
+  //Convert the data for task preview
+  const tasksPreviewData = tasks.map(task => {
+    return ({
+      _id: task._id,
+      title: task.title,
+      subtaskCount: task.subtask.length
+    })
+  })
+
+  //Display task previews on column
+  const tasksPreviewDataSJSX = tasksPreviewData.map(taskPreviewData => {
+    return(
+      <div key={taskPreviewData._id} className="w-72 border border-sky-500">
+        {taskPreviewData.title}
+      </div>
+    )
+  })
+
   return (
-    <div className="w-72 border border-sky-500">
+    <div className="w-72">
       {/* Column Title */}
       <div key={column.index}>{column.title}</div>
       {/* Tasks */}
-      <div>content</div>
+      {tasksPreviewDataSJSX}
     </div>
   )
 }
--- frontend/src/components/Columns/DisplayColumn.tsx
+++ frontend/src/components/Columns/DisplayColumn.tsx
@@ -15,8 +15,8 @@ export default function DisplayColumn({ columns, createNewColumn }: Props) {
 
   const allColumns = columns.map(column => <Column key={column.index} column={column}/>);
   return (
-    <div className="flex flex-row">
-        <div className="flex flex-row">{[...allColumns, <Button key="add_new_column" className="w-72 border border-sky-500" onClick={() => {createNewColumn()}}>Add new column</Button>]}</div>
+    <div className="flex flex-row gap-x-9	">
+        <div className="flex flex-row gap-x-9">{[...allColumns, <Button key="add_new_column" className="w-72 border border-sky-500" onClick={() => {createNewColumn()}}>Add new column</Button>]}</div>
     </div>
     
   )
