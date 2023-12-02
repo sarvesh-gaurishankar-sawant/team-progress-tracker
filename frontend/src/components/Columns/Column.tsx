fcd6cca9119d7524a999ef2bb4938c44299f9d9f
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-12-02 02:15:10 -0500

    Moving the DNDContext out

--- frontend/src/components/Columns/Column.tsx
+++ frontend/src/components/Columns/Column.tsx
@@ -30,63 +30,17 @@ export default function Column({ columnTitle,  tasksObjectArray, index, setTasks
   const tasksIds = filterTasksData.map(task => task._id)
 
   return (
-    <DndContext onDragStart={onDragStart} onDragOver={onDragOver}>
+    
       <div className="w-72">
         {/* Column Title */}
         <div key={index} className="mb-6">{columnTitle}</div>
         {/* Tasks */}
         {<SortableContext items={tasksIds}>{tasksPreviewData}</SortableContext>}
-        {createPortal(
-          <DragOverlay>
-            {activeTask && (
-              <Task
-                task={activeTask}
-              />
-            )}
-          </DragOverlay>,
-          document.body
-        )}
+        
       </div>
-    </DndContext>
+
   )
 
-  function onDragStart(event: DragStartEvent) {
-    if (event.active.data.current?.type === "Task") {
-      setActiveTask(event.active.data.current.task);
-      console.log(activeTask)
-      return;
-    }
-  }
-
-  function onDragOver(event: DragOverEvent) {
-    console.log("On drag over")
-    const { active, over } = event;
-    if (!over) return;
-
-    const activeId = active.id;
-    const overId = over.id;
-
-    if (activeId === overId) return;
-
-    const isActiveATask = active.data.current?.type === "Task";
-    const isOverATask = over.data.current?.type === "Task";
-
-    if (!isActiveATask) return;
-
-    // Dropping a Task over another Task
-    if (isActiveATask && isOverATask) {
-      setTasksObjectArray((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t._id === activeId);
-        const overIndex = tasks.findIndex((t) => t._id === overId);
-
-        if (tasks[activeIndex].status != tasks[overIndex].status) {
-          tasks[activeIndex].status = tasks[overIndex].status;
-          return arrayMove(tasks, activeIndex, overIndex - 1);
-        }
-
-        return arrayMove(tasks, activeIndex, overIndex);
-      });
-    }
-  }
+  
 
 }
--- frontend/src/components/Columns/DisplayColumn.tsx
+++ frontend/src/components/Columns/DisplayColumn.tsx
@@ -2,6 +2,10 @@ import { Button, CircularProgress } from "@mui/material";
 import Column from "./Column";
 import { Board, TaskType } from "../type";
 import { useEffect, useState } from "react";
+import { DndContext, DragOverEvent, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+import Task from "../Tasks/Task";
 
 
 type Column = {
@@ -18,6 +22,8 @@ export default function DisplayColumn({ boardData, createNewColumn }: Props) {
 
   const [tasksObjectArray, setTasksObjectArray] = useState<TaskType[]>([]);
   const [refreshTasksData, setRefreshTasksData ] = useState(true)
+
+  const [activeTask, setActiveTask] = useState<TaskType | null>(null);
 
   useEffect(() => {
     const tasksMongoIds = boardData.tasks;
@@ -54,17 +60,68 @@ export default function DisplayColumn({ boardData, createNewColumn }: Props) {
 
   if(allColumns){
     return (
-      <>
+      <DndContext onDragStart={onDragStart} onDragOver={onDragOver}>
+      <div>
         <div className="flex flex-row gap-x-9	">
         <div className="flex flex-row gap-x-9">{[...allColumns, <Button key="add_new_column" className="w-72 border border-sky-500 h-screen" onClick={() => {createNewColumn()}}>Add new column</Button>]}</div>
         </div>
-      </>
+      </div>
+      {createPortal(
+          <DragOverlay>
+            {activeTask && (
+              <Task
+                task={activeTask}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     )
   }
   else {
     return (
       <div className="flex h-screen"><CircularProgress className="mx-auto self-center"/></div>
     )
+  }
+
+  function onDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === "Task") {
+      setActiveTask(event.active.data.current.task);
+      
+      return;
+    }
+  }
+
+  function onDragOver(event: DragOverEvent) {
+    console.log("On drag over")
+    const { active, over } = event;
+    if (!over) return;
+
+    const activeId = active.id;
+    const overId = over.id;
+
+    if (activeId === overId) return;
+
+    const isActiveATask = active.data.current?.type === "Task";
+    const isOverATask = over.data.current?.type === "Task";
+
+    if (!isActiveATask) return;
+
+    // Dropping a Task over another Task
+    if (isActiveATask && isOverATask) {
+      setTasksObjectArray((tasks) => {
+        const activeIndex = tasks.findIndex((t) => t._id === activeId);
+        const overIndex = tasks.findIndex((t) => t._id === overId);
+
+        if (tasks[activeIndex].status != tasks[overIndex].status) {
+          tasks[activeIndex].status = tasks[overIndex].status;
+          return arrayMove(tasks, activeIndex, overIndex - 1);
+        }
+
+        return arrayMove(tasks, activeIndex, overIndex);
+      });
+    }
   }
  
 }
