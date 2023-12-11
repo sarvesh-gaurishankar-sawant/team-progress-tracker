3ff597c20393ec8115d8f069e2f02e0d7c3f1154
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-11 02:21:52 -0500

    Skeleton for analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,26 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Board from "./components/Boards/Board";
+import PieChart from "./analytics/PieChart";
 
 export default function App() {
+  // return (
+  //   <div>
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/board" element={<HomeScreen />}>
+  //           <Route path=":id" element={<Board />} />
+  //         </Route>
+  //       </Routes>
+  //     </BrowserRouter>
+      
+  //   </div>
+  // )
   return (
+  
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/board" element={<HomeScreen />}>
-            <Route path=":id" element={<Board />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PieChart />
     </div>
-  )
+  );
 }
 
