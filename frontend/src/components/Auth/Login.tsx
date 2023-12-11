2e441f192253d1e0564830c5a323fa1ade828213
Sarvesh Sawant <sawant.sar@northeastern.edu>
2023-12-11 18:17:57 -0500

    Add sign in with google

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,18 +1,35 @@
 
+import { useState } from "react";
 import { auth, googleProvider } from "../../firebase-config";
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+import HomeScreen from "../pages/HomeScreen";
 
 
 function Login() {
 
+    const [isLoggedIn, setisLoggedIn] = useState(
+      false || window.localStorage.getItem('isLoggedIn') === 'true'
+    );
+
+    const [token, setToken] = useState('');
+
+    console.log(`isLoggedIn ${isLoggedIn}`)
+    console.log(`token ${token}`)
+
     
     const signInWithGoogle = async () => {
         try {
-          await signInWithPopup(auth, googleProvider);
+          const result = await signInWithPopup(auth, googleProvider);
+          const user = result.user;
+          const idToken = await user.getIdToken();
+          setToken(idToken)
+          setisLoggedIn(true)
+          window.localStorage.setItem('isLoggedIn', 'true');
         } catch (err) {
           console.error(err);
         }
@@ -21,6 +38,9 @@ function Login() {
     const logout = async () => {
         try {
           await signOut(auth);
+          setisLoggedIn(false)
+          setToken('')
+          window.localStorage.setItem('isLoggedIn', 'false');
         } catch (err) {
           console.error(err);
         }
@@ -28,8 +48,7 @@ function Login() {
 
 	return (
 	<div>
-      <button onClick={signInWithGoogle}> Sign In With Google</button>
-      <button onClick={logout}> Logout </button>
+      {!isLoggedIn ? <button onClick={signInWithGoogle}> Sign In With Google</button> : <Navigate to="/board"/>}
     </div>
 	);
 }
