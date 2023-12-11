301ca9f4e0a8bf1d50f8fdbeaba5a8d1241921a8
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-11 02:27:40 -0500

    Added Graphs

--- frontend/src/analytics/PieChart.tsx
+++ frontend/src/analytics/PieChart.tsx
@@ -1,9 +1,9 @@
 import { FetchData, FetchTasksinColumn} from "../analytics/FetchDataAnalytics";
 import React, { useState, useEffect } from 'react';
 import { Pie, Bar} from 'react-chartjs-2';
-import { Chart, ArcElement, PieController, Tooltip, Legend } from 'chart.js';
+import { Chart, ArcElement, PieController, Tooltip, Legend, CategoryScale, LinearScale, BarElement, BarController } from 'chart.js';
 
-Chart.register(ArcElement, PieController, Tooltip, Legend);
+Chart.register(ArcElement, PieController, Tooltip, Legend, CategoryScale, LinearScale, BarElement, BarController);
 
 export default function PieChart() {
   const boardId = '656b8344a83badbd99125a64';
@@ -33,7 +33,7 @@ export default function PieChart() {
     fetchDataAndTasks();
   }, []);
 
-  const chartData = {
+  const pieChartData = {
     labels: Object.keys(columnData),
     datasets: [
       {
@@ -44,7 +44,20 @@ export default function PieChart() {
           'rgba(54, 162, 235, 0.6)',
           'rgba(255, 206, 86, 0.6)',
           'rgba(75, 192, 192, 0.6)',
+          // Add more colors as needed based on the number of columns
         ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const barChartData = {
+    labels: Object.keys(columnData),
+    datasets: [
+      {
+        label: 'Tasks Count',
+        data: Object.values(columnData),
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
         borderWidth: 1,
       },
     ],
@@ -53,8 +66,17 @@ export default function PieChart() {
   console.log(columnData);
 
   return (
-    <div style={{ width: '400px', height: '400px' }}>
-    <Pie data={chartData} />
-  </div>
+    <div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ width: '45%' }}>
+          <h2>Pie Chart</h2>
+          <Pie data={pieChartData} />
+        </div>
+        <div style={{ width: '45%' }}>
+          <h2>Bar Graph</h2>
+          <Bar data={barChartData} />
+        </div>
+      </div>
+    </div>
   );
 }
