a1ccea252c07946819952e40f6750abeadfd308d
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-10 21:49:54 -0500

    Add Component for Loading Supported Languages

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -9,39 +9,9 @@ import { setSideBarFlag } from "../../store/flags/sideBarFlagSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store"
 import { Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText } from '@mui/material';
-
+import {cols} from '../../components/LoadJson/LoadJson';
 import axios from 'axios';
 
-interface LanguageData {
-  name: string;
-  nativeName: string;
-  dir: string;
-}
-
-interface TranslationData {
-  [key: string]: LanguageData;
-}
-
-const jsonData: { translation: TranslationData } = require('../../assets/JSON/languages.json');
-
-
-
-const languageData = Object.keys(jsonData.translation).map(language_code => ({
-  language_code,
-  display_name: jsonData.translation[language_code].name,
-  native_name: jsonData.translation[language_code].nativeName,
-  direction: jsonData.translation[language_code].dir,
-}));
-
-
-const columnCount = 2;
-const languageDataLength = languageData.length;
-const columnSize = Math.ceil(languageDataLength / columnCount);
-
-const cols: { language_code: string; display_name: string; native_name: string; direction: string; }[][] = [];
-for (let i = 0; i < columnCount; i++) {
-  cols.push(languageData.slice(i * columnSize, (i + 1) * columnSize));
-}
 
 const NavBar: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
