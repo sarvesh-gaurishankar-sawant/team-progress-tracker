4be89ad6d5fcd8b316b7a4d759a968bb606f39b9
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-12 02:34:07 -0500

    random changes

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { ReactComponent as FullLogo } from '../../assets/svg/full-icon.svg';
 import PlusIcon from "../../icons/PlusIcon";
-import { NavLink, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../store/store"
-import CreateNewBoard from "../Boards/CreateNewBoard";
 import DisplayCharts from "../../analytics/DisplayCharts";
 import Board from "../Boards/Board";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store/store"
 import { BoardType } from "../type";
 import ShareBoardModal from "../modals/ShareBoard";
 import { NavLink } from "react-router-dom";
