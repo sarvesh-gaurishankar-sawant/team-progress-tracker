40297cfb1f31355120ec6d93736ba566f1bc2214
Sarvesh Sawant <sawant.sar@northeastern.edu>
2023-12-12 17:11:34 -0500

    Add edit board component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -19,6 +19,7 @@ import AccountMenu from '../Auth/AccountMenu';
 
 
 
+
 const NavBar: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [languageModalOpen, setLanguageModalOpen] = useState<boolean>(false);
@@ -68,6 +69,10 @@ const NavBar: React.FC = () => {
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  // const openEditModal = () => setEditModalOpen(true)
+  // const closeEditModal = () => setEditModalOpen(false)
   
 
   const handleTaskCreationSuccess = () => {
@@ -109,9 +114,7 @@ const NavBar: React.FC = () => {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
               </svg>
             </button>
-            <button>
-              <MoreVertIcon className='h-8' />
-            </button>
+            <MoreVertIcon className='h-8' />
             <AccountMenu />
 
             {/* Language Selection Modal */}
