dc2800c6201462495163fc7c76774b70b613a445
Shashwat Shahi <shahi.sh@northeastern.edu>
2023-12-13 02:04:50 -0500

    add close in task view modal

--- frontend/src/components/modals/task-view-modal.tsx
+++ frontend/src/components/modals/task-view-modal.tsx
@@ -139,12 +139,15 @@ const TaskViewModal: React.FC<TaskViewModalProps> = ({ initialTask, initialSubta
           <Typography className="text-white text-lg">
             {task?.title}
           </Typography>
+          <div className='flex items-center justify-between'>
           <IconButton className="align-top" aria-controls={openMenu ? 'task-action-menu' : undefined} aria-haspopup="true"
             aria-expanded={openMenu ? 'true' : undefined} onClick={handleMenuClick}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
             </svg>
           </IconButton>
+          <button type="button" className="ml-4 text-gray-400 hover:text-red-500" onClick={() => setOpen(false)}>&#x2715;</button>
+          </div>
           <Menu
             id="task-action-menu"
             anchorEl={anchorEl}
